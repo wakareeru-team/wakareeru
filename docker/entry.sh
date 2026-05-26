60315aff2cda9c8bc2483d606e7d17d8c0b8e9fc
SniperPigeon <fengyukunfyk@gmail.com>
2026-05-27 00:36:45 +0800

    Use environment var to config rclone

--- docker/entry.sh
+++ docker/entry.sh
@@ -2,9 +2,14 @@
 set -eu
 
 : "${HF_TOKEN:?HF_TOKEN is required}"
-: "${r2_access_id:?r2_access_id is required}"
-: "${r2_access_key:?r2_access_key is required}"
-: "${r2_endpoint:?r2_endpoint is required}"
+
+R2_ACCESS_ID="${R2_ACCESS_ID:-${r2_access_id:-}}"
+R2_ACCESS_KEY="${R2_ACCESS_KEY:-${r2_access_key:-}}"
+R2_ENDPOINT="${R2_ENDPOINT:-${r2_endpoint:-}}"
+
+: "${R2_ACCESS_ID:?R2_ACCESS_ID is required}"
+: "${R2_ACCESS_KEY:?R2_ACCESS_KEY is required}"
+: "${R2_ENDPOINT:?R2_ENDPOINT is required}"
 
 export HF_HOME="${HF_HOME:-/workspace/.cache/huggingface}"
 export HF_HUB_CACHE="${HF_HUB_CACHE:-$HF_HOME/hub}"
@@ -13,12 +18,12 @@ export XDG_CACHE_HOME="${XDG_CACHE_HOME:-/workspace/.cache}"
 
 mkdir -p "$HF_HOME" "$HF_HUB_CACHE" "$TRANSFORMERS_CACHE" "$XDG_CACHE_HOME"
 
-rclone config create r2 s3 \
-  provider Cloudflare \
-  access_key_id "$r2_access_id" \
-  secret_access_key "$r2_access_key" \
-  endpoint "$r2_endpoint" \
-  region auto \
-  acl private \
+export RCLONE_CONFIG_R2_TYPE=s3
+export RCLONE_CONFIG_R2_PROVIDER=Cloudflare
+export RCLONE_CONFIG_R2_ACCESS_KEY_ID="$R2_ACCESS_ID"
+export RCLONE_CONFIG_R2_SECRET_ACCESS_KEY="$R2_ACCESS_KEY"
+export RCLONE_CONFIG_R2_ENDPOINT="$R2_ENDPOINT"
+export RCLONE_CONFIG_R2_REGION=auto
+export RCLONE_CONFIG_R2_ACL=private
 
 exec "$@"
